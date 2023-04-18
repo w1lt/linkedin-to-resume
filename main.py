fc1f14de5d3b9625db5d2a7fd7beb9688e6f4c15
Will Whitehead <willclashtest@gmail.com>
2023-04-17 22:08:05 -0500

    added name to pdf

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ from linkedin_api import Linkedin
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
-
+dimensions = (612, 792)
 def get_details():
     logininfo = []
-    file = open('login-info', "r")
+    file = open('login-info.txt', "r")
     for line in file:
         logininfo.append(line.split()[1])
     return logininfo
 
 logindetails = get_details()
+print(logindetails)
 # Authenticate using any Linkedin account credentials
 api = Linkedin(logindetails[0], logindetails[1])
-
-# GET a profile
-# GET a profiles contact info
-contact_info = api.get_profile_contact_info('willwhitehead122')
-
-# GET 1st degree connections of a given profile
 profile = api.get_profile("willwhitehead122")
+
+NAME = profile["firstName"] + " " +profile["lastName"]
 experience = profile["experience"]
 print(profile)
     
@@ -31,7 +28,7 @@ def form(path):
     my_canvas.drawString(30, 735, 'OF ACME INDUSTRIES')
     my_canvas.drawString(500, 750, "12/12/2010")
     my_canvas.line(480, 747, 580, 747)
-    my_canvas.drawString(275, 725, 'AMOUNT OWED:')
+    my_canvas.drawString(250, 760, NAME)
     my_canvas.drawString(500, 725, "$1,000.00")
     my_canvas.line(378, 723, 580, 723)
     my_canvas.drawString(30, 703, 'RECEIVED BY:')
